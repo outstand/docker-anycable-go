5bad58c40356fd3649cb21df7e222450d2efcb81
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-06-21 13:09:29 -0700

    Add internal CA root cert to container at runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,6 +14,15 @@ consul_kv() {
     | jq -r ".[0].Value" | base64 -d -
 }
 
+consul_kv_exists() {
+  declare key="$1"
+  local retval=0
+  curl --fail -s ${CONSUL_HOST}:8500/v1/kv/${key} &> /dev/null \
+    || retval=$?
+
+  echo ${retval}
+}
+
 if [ -z "${ANYCABLE_REDIS_URL:-}" ]; then
   echo "Setting redis url to local instance ip!"
   export ANYCABLE_REDIS_URL="redis://$(instance_ip):6379/"
@@ -24,6 +33,19 @@ if [ -z "${ANYCABLE_ALLOWED_ORIGINS:-}" ]; then
   echo "Fetching allowed origins from consul"
   export ANYCABLE_ALLOWED_ORIGINS=$(consul_kv anycable-websocket/allowed_origins | jq -r '. | join(",")')
   echo "ANYCABLE_ALLOWED_ORIGINS=${ANYCABLE_ALLOWED_ORIGINS}"
+fi
+
+internal_ca_exists=$(consul_kv_exists anycable-websocket/internal_ca)
+if [ "${internal_ca_exists}" -eq 0 ]; then
+  echo "Internal CA detected."
+  internal_ca=$(consul_kv anycable-websocket/internal_ca)
+  echo "${internal_ca}" > /usr/local/share/ca-certificates/internal_ca.crt
+  chmod 644 /usr/local/share/ca-certificates/internal_ca.crt
+fi
+
+if [ -n "$(ls -A /usr/local/share/ca-certificates)" ]; then
+  echo "Updating ca-certificates"
+  update-ca-certificates
 fi
 
 set -- /usr/local/bin/anycable-go "$@"
